404b2d7ae5c4dd70db2931d2ae74323c967ed51b
Mohamed <mohasalanka2020@gmail.com>
2023-07-04 06:38:11 +0300

    completed Cookies in APIs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    def index
+        session[:session_hello] ||= "World"
+        cookies[:cookies_hello] ||= "World"
+        render json: { session: session, cookies: cookies.to_hash }
+    end
+      
 end
