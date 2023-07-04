940f0e73518078b1ab09812bdf2e88a1ec63893a
Mohamed <mohasalanka2020@gmail.com>
2023-07-04 06:39:26 +0300

    semi complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::API
-    def index
-        session[:session_hello] ||= "World"
-        cookies[:cookies_hello] ||= "World"
-        render json: { session: session, cookies: cookies.to_hash }
-    end
-      
+    
 end
